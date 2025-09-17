5a103f1f3919b5931811b896cca090afe4a51055
ArsenyStahkov <st96ars@gmail.com>
2025-09-17 22:48:11 +0500

    GetNpcs method has been added

--- Monsters/SkillFileData/SkillData.cs
+++ Monsters/SkillFileData/SkillData.cs
@@ -1,5 +1,6 @@
 ﻿using TheGame.Monsters;
 using TheGame.Monsters.Zombies;
+using System.Text.RegularExpressions;
 
 namespace TheGame.SkillFileData;
 
@@ -9,6 +10,11 @@ public class SkillData
     private const string _npcs = "NPCs";
     private const string _weapons = "WEAPONS";
     private const string _chargeDistribution = "HEALTH/ARMOR CHARGE DISTRIBUTION";
+
+    private const string _health = "health";
+    private const string _dmg = "dmg";
+
+    private const int _wordsCount = 4;
 
     private string[] _fileLines;
 
@@ -26,41 +32,82 @@ public class SkillData
         }
     }
 
-    //public List<NPC>? GetNpcs()
-    //{
-    //    if (_fileLines.Length < 2)
-    //        return null;
+    public List<NPC>? GetNpcs()
+    {
+        if (_fileLines.Length < 2)
+            return null;
 
-    //    int npcsIndex = 0;
+        int npcsIndex = 0;
 
-    //    for (int i = 0; i < _fileLines.Length; i++)
-    //    {
-    //        if (_fileLines[i].Contains(_npcs, StringComparison.OrdinalIgnoreCase))
-    //        {
-    //            npcsIndex = i;
-    //            break;
-    //        }
-    //    }
+        for (int i = 0; i < _fileLines.Length; i++)
+        {
+            if (_fileLines[i].Contains(_npcs, StringComparison.OrdinalIgnoreCase))
+            {
+                npcsIndex = i;
+                break;
+            }
+        }
 
-    //    if (npcsIndex == 0)
-    //        return null;
+        if (npcsIndex == 0)
+            return null;
 
-    //    for (int i = npcsIndex + 1; i < _fileLines.Length; i++)
-    //    {
-    //        if (_fileLines[i].Contains("//"))
-    //        {
-    //            if (_fileLines[i].Contains(nameof(Zombie), StringComparison.OrdinalIgnoreCase)
-    //                || _fileLines[i].Contains(nameof(Imp), StringComparison.OrdinalIgnoreCase)
-    //                || _fileLines[i].Contains(nameof(Demon), StringComparison.OrdinalIgnoreCase)
-    //                )
-    //            {
+        List<NPC> NPCs = new List<NPC>();
+        int initIndex = npcsIndex + 1;
 
-    //            }
-    //        }
-    //    }
+        for (int i = initIndex; i < _fileLines.Length; i++)
+        {
+            if (!_fileLines[i].Contains("//"))
+                continue;
 
-    //    //List<NPC> NPCs = new List<NPC>();
+            bool isNpc = _fileLines[i].Contains(nameof(Zombie), StringComparison.OrdinalIgnoreCase)
+                      || _fileLines[i].Contains(nameof(Imp), StringComparison.OrdinalIgnoreCase)
+                      || _fileLines[i].Contains(nameof(Demon), StringComparison.OrdinalIgnoreCase);
 
-    //    //return NPCs;
-    //}
+            if (isNpc)
+            {
+                NPC npc = new NPC();
+
+                for (int k = i + 1; !_fileLines[k].Contains("//"); k++)
+                {
+                    i = k;
+
+                    if (string.IsNullOrEmpty(_fileLines[k]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        _fileLines[k] = Regex.Replace(_fileLines[k], @"\s+", "_");
+                        string[] skill = _fileLines[k].Split('_');
+
+                        if (skill.Length != _wordsCount)
+                            continue;
+
+                        npc.Name = skill[1].ToLower();
+                        string value = skill[3].Replace("\"", string.Empty);
+
+                        ushort health;
+                        if (skill[2].ToLower() == _health && UInt16.TryParse(value, out health))
+                        {
+                            npc.Health = health;
+                            continue;
+                        }
+
+                        float dmg;
+                        if (skill[2].ToLower() == _dmg && Single.TryParse(value, out dmg))
+                            npc.Dmg = dmg > 0 ? dmg : 0;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(npc.Name))
+                    NPCs.Add(npc);
+            }
+            else if (_fileLines[i].Contains("=") && i != initIndex)
+            {
+                break;
+            }
+        }
+
+        return NPCs;
+    }
 }
